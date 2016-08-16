1255cf66d143ef6601fcb59a0f745b3ec48f8e00
kana030 <kana.o030o@gmail.com>
2016-08-16 20:08:26 +0900

    login

---
+++ www/js/app.js
@@ -0,0 +1,59 @@
+var appKey    = "e1831388d9e33026596e7908b2fc08a8f9f69d5115a52e5fc5c312c7b6c26418";
+var clientKey = "2ecfc4e637dc5369f2ae79aa975d689785da857c9241fba3db2401a214092bbc";
+var ncmb = new NCMB(appKey, clientKey);
+
+///// Called when app launch
+$(function() {
+  $("#LoginBtn").click(onLoginBtn);
+  $("#RegisterBtn").click(onRegisterBtn);
+  $("#YesBtn_logout").click(onLogoutBtn);
+});
+
+//----------------------------------USER MANAGEMENT-------------------------------------//
+var currentLoginUser; //現在ログイン中ユーザー
+
+function onRegisterBtn()
+{
+    //入力フォームからusername, password変数にセット
+    var username = $("#reg_username").val();
+    var password = $("#reg_password").val();
+    
+    var user = new ncmb.User();
+    user.set("userName", username)
+        .set("password", password);
+    
+    // 任意フィールドに値を追加 
+    user.signUpByAccount()
+        .then(function(user) {
+            alert("新規登録に成功");
+            currentLoginUser = ncmb.User.getCurrentUser();
+            $.mobile.changePage('#DetailPage');
+        })
+        .catch(function(error) {
+            alert("新規登録に失敗！次のエラー発生：" + error);
+        });
+}
+
+function onLoginBtn()
+{
+    var username = $("#login_username").val();
+    var password = $("#login_password").val();
+    // ユーザー名とパスワードでログイン
+    ncmb.User.login(username, password)
+        .then(function(user) {
+            alert("ログイン成功");
+            currentLoginUser = ncmb.User.getCurrentUser();
+            $.mobile.changePage('#DetailPage');
+        })
+        .catch(function(error) {
+            alert("ログイン失敗！次のエラー発生: " + error);
+        });
+}
+
+function onLogoutBtn()
+{
+    ncmb.User.logout();
+    alert('ログアウト成功');
+    currentLoginUser = null;
+    $.mobile.changePage('#LoginPage');
+}
